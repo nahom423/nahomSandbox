32ce781b11d8cc19414622ac7ea09fd00740f821
nahom423 <nnegash@ifmrestoration.com>
2020-08-05 05:42:42 +0000

    Use sql to execute database  queries on the database

--- contactList/main.go
+++ contactList/main.go
@@ -8,9 +8,9 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 type ContactList struct { //creating struct
-	id int
-	name string
-	address string
+	Id int
+	Name string
+	Address string
 }
 
 func db() (db *sql.DB){ //establishing db connection
@@ -24,6 +24,33 @@ func db() (db *sql.DB){ //establishing db connection
 	}
 	return db
 }
+
+var tmpl = template.Must(template.ParseGlob("form/*"))
+
+func Index(w http.ResponseWriter, r *http.Request){
+	db := dbConn()
+	selDB, err := db.Query("Select * From Contact_List ORDER BY id DESC" )
+	if err !nil {
+		panic(err.Error())
+	}
+	list := ContactList{}
+	res := []ContactList{}
+
+	for selDB.Next(){
+		var id int
+		var name, address string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		list.Id = id
+		list.Name = name
+		list.Address = address
+		res = append (res, emp)
+	}
+	tmpl.ExecuteTemplate(w,"Index", res)
+	defer db.Close()
+}
 func main() {
-	
+
 }
