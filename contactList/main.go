ab8857bc8687989f2d06c72eff4ccd4ed687f796
nahom423 <nnegash@ifmrestoration.com>
2020-08-05 05:02:47 +0000

    Creating struct & Establishing db connection

--- contactList/main.go
+++ contactList/main.go
@@ -1,5 +1,29 @@
 package main
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"text/template"
 
+	_ "github.com/go-sql-driver/mysql"
+)
+type ContactList struct { //creating struct
+	id int
+	name string
+	address string
+}
+
+func db() (db *sql.DB){ //establishing db connection
+	dbDriver := "mysql"
+	dbUser:= "nnegash"
+	dbPass:= "1234"
+	dbName:= "IFM_Contact_List"
+	db, err := sql.Open(dbDriver, dbUser + ":" +dbPass+"@/"+dbName )
+	if err!= nil{
+		panic(err.Error())
+	}
+	return db
+}
 func main() {
 	
 }
